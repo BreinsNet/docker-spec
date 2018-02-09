6ed90d1f131968532f12b0d057d8d00c69832eae
Juan Breinlinger <juan.brein@breins.net>
2018-02-09 09:40:30 +0100

    prevent tar to clear clache

--- lib/docker/spec.rb
+++ lib/docker/spec.rb
@@ -124,8 +124,8 @@ cp -r root $TMPDIR/root && cd $TMPDIR &&
 sudo chown root:root -R root &&
 (cd root && sudo tar cf ../root.tar .) &&
 sudo chown -R `id -u`:`id -g` root.tar &&
-cp root.tar $WD &&
-sudo  rm -rf $TMPDIR'
+cp root.tar $WD && touch -t 200001010000.00 root.tar
+sudo rm -rf $TMPDIR'
 EOF
     system command if Dir.exist?(ROOT_DIR)
   end
